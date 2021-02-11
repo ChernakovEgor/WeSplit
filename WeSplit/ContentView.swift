a4ed4048b7b5c9385f8353dfd7d2610421f8a516
Egor Chernakov <chernakov.eg@gmail.com>
2021-02-11 18:55:40 +0300

    Project Complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                     } .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
+                Section(header: Text("Amount per person")) {
                     Text("\(totalPerPerson, specifier: "%.2f")₽")
                 }
             }
